7b31e1c499a53b098cdb717295349d9f8ddaa124
Garrett Michael Flynn <46533749+GarrettMFlynn@users.noreply.github.com>
2021-11-12 17:19:47 -0800

    Converted for brain control

---
+++ README.md
@@ -0,0 +1,2 @@
+# derpy-frog
+Derp derp derp.
---
+++ app/settings.js
@@ -0,0 +1,66 @@
+import {UI} from "./UI.js"
+
+let button = document.createElement('button')
+button.style = 'position: absolute; bottom: 0; right: 0; z-index: 1;'
+button.innerHTML = 'Connect Device'
+document.body.insertAdjacentElement('beforebegin', button)
+
+export const settings = {
+  name: "Derpy Frog",
+  devices: [
+    "EEG"
+  ],
+  author: "Autumn Auriel",
+  description: "A staring contest (with yourself...)",
+  categories: [
+    "train"
+  ],
+  graphs: [
+    {
+      nodes: [
+        {
+          name: "blink_left",
+          class: "Event",
+          "params": {
+            "keycode": "Space"
+          },
+        },
+        {
+          name: "blink_right",
+          class: "Event",
+          params: {
+            "keycode": "Space"
+          },
+        },
+        {
+          name: "ui",
+          class: UI,
+          params: {},
+        },
+        {
+          name: "document",
+          class: "DOM",
+          params: {},
+        }
+      ],
+      edges: [
+        {
+          source: 'blink_left',
+          target: 'ui:jump'
+        },
+        {
+          source: 'blink_right',
+          target: 'ui:jump'
+        },
+        {
+          source: 'ui:element',
+          target: 'document:content'
+        },
+      ]
+    }
+  ],
+  "version": "0.0.38",
+  "connect": {
+    toggle: button
+  }
+};
